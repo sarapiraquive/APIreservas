395e23924be764433c6f06d68c031ab350f504ef
sarapiraquive <sarapirpa@unisabana.edu.co>
2024-03-21 13:00:12 -0500

    Crear pruebas unitarias de ClienteService

---
+++ src/test/java/co/edu/unisabana/ReservaCitas/services/CitaServiceTest.java
@@ -0,0 +1,37 @@
+package co.edu.unisabana.ReservaCitas.services;
+
+import co.edu.unisabana.ReservaCitas.entidades.Cita;
+import co.edu.unisabana.ReservaCitas.entidades.Cliente;
+import co.edu.unisabana.ReservaCitas.entidades.Estilista;
+import co.edu.unisabana.ReservaCitas.entidades.Servicio;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class CitaServiceTest {
+
+    @Test
+    void getAllCitas() {
+    }
+
+    @Test
+    void getCitaById() {
+    }
+
+    @Test
+    void crearCita() {
+    }
+
+    @Test
+    void eliminarCita() {
+    }
+
+    @Test
+    void actualizarFechaCita() {
+    }
+}
