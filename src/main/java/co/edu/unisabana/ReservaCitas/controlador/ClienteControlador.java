f0a3471b35359c76114ee192ccd922a0c5519423
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2024-04-02 07:54:27 -0500

    XYZ

--- src/main/java/co/edu/unisabana/ReservaCitas/controlador/ClienteControlador.java
+++ src/main/java/co/edu/unisabana/ReservaCitas/controlador/ClienteControlador.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/clientes")
 @Api(tags = "Clientes", description = "Operaciones de clientes")
-public class ClienteControlador {
+public class ClienteControlador
 
     private final ClienteService clienteService;
 
