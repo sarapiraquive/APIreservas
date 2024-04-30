0c47257c256e5457e5dc22ad5e0aaaf2725da00b
sarapiraquive <sarapirpa@unisabana.edu.co>
2024-04-29 21:34:42 -0500

    Actualizar dockerfile

--- src/main/java/co/edu/unisabana/ReservaCitas/ReservaCitasApplication.java
+++ src/main/java/co/edu/unisabana/ReservaCitas/ReservaCitasApplication.java
@@ -4,7 +4,9 @@ import co.edu.unisabana.ReservaCitas.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @Import(SwaggerConfig.class)
 public class ReservaCitasApplication {
@@ -15,3 +17,4 @@ public class ReservaCitasApplication {
 
 
 }
+ 	
